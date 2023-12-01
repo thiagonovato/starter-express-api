ae64d225de1578855f2150f1e865b96f3020e8ec
Thiago Novato <thiagonovato@gmail.com>
2023-12-01 12:35:19 -0500

    First deploy

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Olá Mundo!')
+})
+
+app.post("/api", (req, res) => {
+    console.log(req.body)
+    const result = {
+        status: "ok",
+        code: 200,
+        date: new Date(),
+        message: req.body
+    }
+
+    res.send(result)
 })
 app.listen(process.env.PORT || 3000)
